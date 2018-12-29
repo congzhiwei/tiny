6492ae7a3730e495785ea9dac98d2ba38e0f10de
congzhiwei@taihe.com <congzhiwei@taihe.com>
2018-12-29 16:46:46 +0800

    添加

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 module.exports = function tiny(string) {
-  if (typeof string !== "string") throw new TypeError("Tiny wants a string1!");
+  if (typeof string !== "string") throw new TypeError("Tiny wants a string2!");
   return string.replace(/\s/g, "");
 };
